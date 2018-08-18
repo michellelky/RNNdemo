9437c10e83513763f7c809fdfd32b56050302d84
Michelle Lau <michellelkys@gmail.com>
2018-08-18 18:40:26 +0800

    Add scrollview to home

--- src/screens/Deck.tsx
+++ src/screens/Deck.tsx
@@ -1,26 +1,58 @@
 import * as React from 'react';
 import {
-    Platform, 
     StyleSheet, 
     Text, 
-    View
+    View,
+    ScrollView,
+    Image,
+    Dimensions
 } from 'react-native';
 
-const instructions = Platform.select({
-  ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
-  android:
-    'Double tap R on your keyboard to reload,\n' +
-    'Shake or press menu button for dev menu',
-});
+const {height, width} = Dimensions.get('window');
 
 type Props = {};
 export default class App extends React.Component<Props> {
   render() {
     return (
       <View style={styles.container}>
-        <Text style={styles.welcome}>Welcome to React Native!</Text>
-        <Text style={styles.instructions}>To get started, edit App.js</Text>
-        <Text style={styles.instructions}>{instructions}</Text>
+       <Text style={[{textAlign: 'left', fontSize: 24}]}>Using native ScrollView</Text>
+
+        <ScrollView 
+          horizontal={true}
+          snapToInterval={width - 40} // card width offset margin
+          snapToAlignment={'center'}
+          decelerationRate={0} // stop scrolling momentum
+          >
+            {/* dummy cards */}
+            <View style={styles.card}>
+                <Image style={styles.avatar} source={ {uri: 'https://i.scdn.co/image/3d7ad07f25e3d56c09eefc01a330e346bcd4f793'} } />
+                <Text style={styles.text}>
+                  Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam.
+                </Text>
+            </View>
+
+            <View style={styles.card}>
+                <Image style={styles.avatar} source={ {uri: 'https://i.scdn.co/image/3d7ad07f25e3d56c09eefc01a330e346bcd4f793'} } />
+                <Text style={styles.text}>
+                  Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam.
+                </Text>
+            </View>
+
+            <View style={styles.card}>
+                <Image style={styles.avatar} source={ {uri: 'https://i.scdn.co/image/3d7ad07f25e3d56c09eefc01a330e346bcd4f793'} } />
+                <Text style={styles.text}>
+                  Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam.
+                </Text>
+            </View>
+
+            <View style={styles.card}>
+                <Image style={styles.avatar} source={ {uri: 'https://i.scdn.co/image/3d7ad07f25e3d56c09eefc01a330e346bcd4f793'} } />
+                <Text style={styles.text}>
+                  Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam.
+                </Text>
+            </View>
+        </ScrollView>
+
       </View>
     );
   }
@@ -33,14 +65,29 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     backgroundColor: '#F5FCFF',
   },
-  welcome: {
+  card: {
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#FFF4ED',
+    padding: 30,
+
+    borderRadius: 15,
+    shadowColor: '#000',
+    shadowOffset: { width: 2, height: 2 },
+    shadowOpacity: 0.4,
+    shadowRadius: 6,
+    width: width * 0.8, // percent or minus
+    height: height * 0.7,
+    margin: 20,
+  },
+  avatar: {
+    width: 300,
+    height: 300
+  },
+  text: {
     fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
     color: '#333333',
-    marginBottom: 5,
+    paddingTop: 20,
   },
+  
 });
